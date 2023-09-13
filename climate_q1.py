20f61540c5a4d7de4d73d4e761f46c51f936fd4c
vydang123 <140564196+vydang123@users.noreply.github.com>
2023-09-13 13:24:47 +1000

    Added database connection and data fetching code to climate_q1.py

--- climate_q1.py
+++ climate_q1.py
@@ -1,18 +1,37 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
 years = []
 co2 = []
 temp = []
 
+# Connect to the database
+conn = sqlite3.connect('climate.db')
+cursor = conn.cursor()
+
+# Fetch data from the database
+cursor.execute("SELECT Year, CO2, Temperature FROM ClimateData")
+rows = cursor.fetchall()
+
+for row in rows:
+    years.append(row[0])
+    co2.append(row[1])
+    temp.append(row[2])
+
+# Close the connection
+conn.commit()
+conn.close()
+
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, co2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, temp, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+
 plt.show() 
 plt.savefig("co2_temp_1.png") 
